f1be7c691ad7abd3bee4fd0f576cbb69f9e3018e
Joe Doe <svtgomez@gmail.com>
2019-02-18 01:19:32 -0800

    Split into current and previous tickers

--- portfolio_viewer/views.py
+++ portfolio_viewer/views.py
@@ -1,29 +1,26 @@
 import json
 
-from datetime import datetime
-
 from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import render
 
 
 class PortfolioToolsException(Exception):
     pass
+
 
 def get_tickers():
     portfolio_file = '{0}/data/portfolio.json'.format(settings.BASE_DIR)
     print('Importing data from file: {}'.format(portfolio_file))
 
     json_stream = open(portfolio_file).read()
-    json_data = json.loads(json_stream)
-    return json_data['data']
+    return json.loads(json_stream)
 
 
-def get_ticker_url(ticker, term='short', type='chart'):
+def get_ticker_url(ticker, term='short', resource_type='chart'):
     """
     :param ticker: Stock symbol for the company
     :param term: short, medium, or long term scale
-    :param type: chart or quote
+    :param resource_type: chart or quote
     :return: url for the image or web page
     """
     '''
@@ -33,12 +30,12 @@ def get_ticker_url(ticker, term='short', type='chart'):
     if term.strip().lower() not in {'short', 'medium', 'long'}:
         raise PortfolioToolsException('Ticker url term duration \'{0}\' invalid'.format(term))
 
-    if type.strip().lower() not in {'quote', 'chart'}:
-        raise PortfolioToolsException('Ticker type \'{0}\' invalid'.format(type))
+    if resource_type.strip().lower() not in {'quote', 'chart'}:
+        raise PortfolioToolsException('Ticker type \'{0}\' invalid'.format(resource_type))
 
-    short_term = 'https://finviz.com/{0}.ashx?t={1}&ty=c&ta=1&p=d&s=l'.format(type, ticker)  # 4-5 mo w/ trends
-    medium_term = 'https://finviz.com/{0}.ashx?t={1}'.format(type, ticker)  # 9 months
-    long_term = 'https://finviz.com/{0}.ashx?t={1}&ty=c&ta=0&p=w'.format(type, ticker)  # 24 months
+    short_term = 'https://finviz.com/{0}.ashx?t={1}&ty=c&ta=1&p=d&s=l'.format(resource_type, ticker)  # 4-5 mo w/ trends
+    medium_term = 'https://finviz.com/{0}.ashx?t={1}'.format(resource_type, ticker)  # 9 months
+    long_term = 'https://finviz.com/{0}.ashx?t={1}&ty=c&ta=0&p=w'.format(resource_type, ticker)  # 24 months
 
     if term.strip().lower() == 'short':
         return short_term
@@ -48,31 +45,40 @@ def get_ticker_url(ticker, term='short', type='chart'):
         return long_term
 
 
-def index(request):
-    # Get tickers
-    tickers = get_tickers()
-
-    import pytz
-
-    # Build html
-    html_contents = '<h2>Portfolio viewer</h2>' \
-        'Time: {0}' \
-        '<table style="width:100%" border="1">' \
-        '  <tr>' \
-        '    <th>Short term</th>' \
-        '    <th>Long term</th>' \
-        '  </tr>'.format(str(datetime.now(pytz.timezone('US/Pacific')).strftime("%m-%d-%Y %H:%M %p")))
+def get_ticker_table(table_title, tickers):
+    html_contents = '<h2>{}</h2>\n' \
+                    '<table style="width:100%" border="1">\n' \
+                    '  <tr>\n' \
+                    '    <th>Short term</th>\n' \
+                    '    <th>Long term</th>\n' \
+                    '  </tr>\n'.format(table_title)
 
     for ticker in tickers:
         info_url = get_ticker_url(ticker, 'medium', 'quote')
         short_image = get_ticker_url(ticker, 'short')
         long_image = get_ticker_url(ticker, 'long')
-        html_contents += '<tr><td><a href={0}>{1}</a></td></tr>'.format(info_url, info_url)
+        html_contents += '<tr><td><a href={0}>{1}</a></td></tr>\n'.format(info_url, info_url)
         html_contents += '' \
-        '  <tr>' \
-        '    <td><img src={0}></td>' \
-        '    <td><img src={1}></td>' \
-        '  </tr><tr></tr>'.format(short_image, long_image)
+                         '  <tr>\n' \
+                         '    <td><img src={0}></td>\n' \
+                         '    <td><img src={1}></td>\n' \
+                         '  </tr><tr></tr>\n'.format(short_image, long_image)
+    html_contents += '</table>\n'
+
+    return html_contents
+
+
+def index(request):
+    # Get tickers
+
+    tickers = get_tickers()
+    current_tickers = tickers['current_tickers']
+    previous_tickers = tickers['previous_tickers']
+
+    # Build html
+    html_contents = '<h1>Portfolio viewer</h1>\n'
+    html_contents += get_ticker_table("Current tickers", current_tickers)
+    html_contents += get_ticker_table("Previous tickers", previous_tickers)
 
     print('html_contents: {}'.format(html_contents))
     return HttpResponse(html_contents)
